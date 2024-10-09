5a85a7940578ce166904390b578e82d5a749a872
rikvart <vartiainenriku@gmail.com>
2024-10-09 14:36:41 +0300

    task 34

--- part04-34_most_common_character/src/most_common_character.py
+++ part04-34_most_common_character/src/most_common_character.py
@@ -1,1 +1,20 @@
 # Write your solution here
+
+def most_common_character(string: str):
+
+    i = 0
+    largest = 0
+    letter = ""
+    for char in string:
+        if string.count(char) > largest:
+            largest = string.count(char)
+            letter = char
+        
+        
+    return letter
+if __name__ == "__main__":
+
+    stringToSend = "aabcdfgggh"
+    most_common_character(stringToSend)
+
+        
