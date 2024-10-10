c90997b95dc254599246d02910424213eacd17e0
rikvart <vartiainenriku@gmail.com>
2024-10-10 09:07:34 +0300

    tasks

--- part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,14 @@
 # Write your solution here
+
+def no_vowels(string: str):
+    newString = ""
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    for char in string:
+        if char in vowels:
+            continue
+        else: 
+            newString = newString + char
+
+    return newString    
+
+        
