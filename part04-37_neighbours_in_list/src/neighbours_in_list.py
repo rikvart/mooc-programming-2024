e2f7701640f3fe7a91c49dd1bb9cfc186ddd6594
rikvart <vartiainenriku@gmail.com>
2024-10-10 09:39:06 +0300

    tasks

--- part04-37_neighbours_in_list/src/neighbours_in_list.py
+++ part04-37_neighbours_in_list/src/neighbours_in_list.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def longest_series_of_neighbours(my_list: list):
+    new_list = []
+
+    for item in my_list:
+        print (item)
+
+
+if __name__ == "__main__":
+
+    testlist = [1, 2, 5, 7, 6, 5, 6, 3, 4, 1, 0]
+    longest_series_of_neighbours(testlist)
