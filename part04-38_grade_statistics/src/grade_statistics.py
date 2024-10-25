7b87cd03c4fd17a0294729ca679a79f76ccb5e26
rikvart <vartiainenriku@gmail.com>
2024-10-25 12:06:31 +0300

    work on 4.38

--- part04-38_grade_statistics/src/grade_statistics.py
+++ part04-38_grade_statistics/src/grade_statistics.py
@@ -6,20 +6,20 @@ def input_from_user():
         data = input("Exam points and exercises completed:")
         if data != "":
             exam_points, exercises = map(int, data.split())
-            numbers.append({"exam_points": exam_points, "exercises": exercises})
+            numbers.append(
+                {"exam_points": exam_points, "exercises": exercises})
         else:
             break
 
-        
     return numbers
+
 
 def statistics(stats: list):
 
     grades = []
     total_for_div = 0
     average = total_for_div / len(stats)
-    
-    
+
     for object in stats:
         current_exam = object['exam_points']
         current_excercises = math.floor(object['exercises'] / 10)
@@ -39,35 +39,29 @@ def statistics(stats: list):
         elif current_total <= 30 and current_total >= 30:
             grades.append(5)
 
-    
+    average = total_for_div / len(stats)
 
+    print("Statistics")
+    print("Points average: " + str(average))
 
-        
-        print("exam score: " + str(object['exam_points']) + " excercises: " + str(object['exercises']) + "current" +str(current_excercises))
-    total_for_div = total_for_div + current_total
-    
-    print(grades, total_for_div, average)
-
-    
     return stats
+
 
 if __name__ == "__main__":
 
     print(statistics(input_from_user()))
 
 
-
 # The exercises completed are converted into exercise points, so that completing at least 10% of the exercises grants one point
-# 20% grants two points, and so forth. Completing all 100 exercises grants 10 exercise points. 
+# 20% grants two points, and so forth. Completing all 100 exercises grants 10 exercise points.
 # The number of exercise points granted is an integer value, rounded down.
 # Statistics:
-#Points average: 14.5
-#Pass percentage: 75.0
-#Grade distribution:
+# Points average: 14.5
+# Pass percentage: 75.0
+# Grade distribution:
 #  5:
 #  4:
 #  3: *
 #  2:
 #  1: **
 #  0: *
-
