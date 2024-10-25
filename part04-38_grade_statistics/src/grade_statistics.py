77749c946ca5d8b91662c8d2a63a0a1fcb3e17e1
rikvart <vartiainenriku@gmail.com>
2024-10-25 12:34:51 +0300

    part 4 done

--- part04-38_grade_statistics/src/grade_statistics.py
+++ part04-38_grade_statistics/src/grade_statistics.py
@@ -1,31 +1,27 @@
 import math
 
-def input_from_user():
-    numbers = []
-    while True:
-        data = input("Exam points and exercises completed:")
-        if data != "":
-            exam_points, exercises = map(int, data.split())
-            numbers.append(
-                {"exam_points": exam_points, "exercises": exercises})
-        else:
-            break
+numbers = []
+while True:
+    data = input("Exam points and exercises completed:")
+    if data != "":
+        exam_points, exercises = map(int, data.split())
+        numbers.append(
+            {"exam_points": exam_points, "exercises": exercises})
+    else:
+        break
 
-    return numbers
+grades = []
+total_for_div = 0
 
+for object in numbers:
+    current_exam = object['exam_points']
+    current_excercises = math.floor(object['exercises'] / 10)
+    current_total = current_exam + current_excercises
+    total_for_div = total_for_div + current_total
 
-def statistics(stats: list):
-
-    grades = []
-    total_for_div = 0
-    average = total_for_div / len(stats)
-
-    for object in stats:
-        current_exam = object['exam_points']
-        current_excercises = math.floor(object['exercises'] / 10)
-        current_total = current_exam + current_excercises
-        total_for_div = total_for_div + current_total
-
+    if current_exam < 10:
+        grades.append(0)
+    else:
         if current_total <= 14:
             grades.append(0)
         elif current_total <= 17 and current_total >= 15:
@@ -39,29 +35,17 @@ def statistics(stats: list):
         elif current_total <= 30 and current_total >= 30:
             grades.append(5)
 
-    average = total_for_div / len(stats)
+average = total_for_div / len(numbers)
+fails = grades.count(0)
+pass_perc = (len(grades) - fails) / len(grades) * 100
 
-    print("Statistics")
-    print("Points average: " + str(average))
-
-    return stats
-
-
-if __name__ == "__main__":
-
-    print(statistics(input_from_user()))
-
-
-# The exercises completed are converted into exercise points, so that completing at least 10% of the exercises grants one point
-# 20% grants two points, and so forth. Completing all 100 exercises grants 10 exercise points.
-# The number of exercise points granted is an integer value, rounded down.
-# Statistics:
-# Points average: 14.5
-# Pass percentage: 75.0
-# Grade distribution:
-#  5:
-#  4:
-#  3: *
-#  2:
-#  1: **
-#  0: *
+print("Statistics:")
+print("Points average: " + str(average))
+print("Pass percentage: " + str(round(pass_perc, 1)))
+print("Grade distribution: ")
+print((" 5: " + "*" * grades.count(5)))
+print((" 4: " + "*" * grades.count(4)))
+print((" 3: " + "*" * grades.count(3)))
+print((" 2: " + "*" * grades.count(2)))
+print((" 1: " + "*" * grades.count(1)))
+print((" 0: " + "*" * grades.count(0)))
