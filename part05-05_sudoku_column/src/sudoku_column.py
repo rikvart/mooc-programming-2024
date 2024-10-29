9345c8d0ffa1a7e0808087f8943c8d3da8cff2da
rikvart <vartiainenriku@gmail.com>
2024-10-29 16:07:38 +0200

    tasks

--- part05-05_sudoku_column/src/sudoku_column.py
+++ part05-05_sudoku_column/src/sudoku_column.py
@@ -1,1 +1,19 @@
 # Write your solution here
+
+def column_correct(sudoku: list, column_no: int):
+    to_return = True
+    filtered = []
+    for row in sudoku:
+        if row[column_no] != 0:
+            filtered.append(row[column_no])
+        
+    for num in filtered:
+        if filtered.count(num) < 2:
+            continue
+        else:
+            to_return = False
+        
+    return to_return
+
+
+           
