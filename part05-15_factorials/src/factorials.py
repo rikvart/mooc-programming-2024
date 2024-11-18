ab183083ecac26a5e440ce2d2c2a5a0375f3b552
rikvart <vartiainenriku@gmail.com>
2024-11-18 17:21:49 +0200

    tasks

--- part05-15_factorials/src/factorials.py
+++ part05-15_factorials/src/factorials.py
@@ -1,16 +1,17 @@
-# Write your solution here
 
 def factorials(n: int):
-    i = n
-    total = 0
     my_dictionary = {}
-    if n == 1:
-        total = 1
-    else: 
-        while i > 0:
-            total = n * i
-            i -= 1
-
-    
-    my_dictionary[n] = total
+    for num in range(1, n + 1):
+        factorial = 1
+        for i in range(1, num + 1):
+            factorial *= i
+        my_dictionary[num] = factorial
+    print(my_dictionary)
     return my_dictionary
+
+
+if __name__ == "__main__":
+    k = factorials(5)
+    print(k[1])
+    print(k[3])
+    print(k[5])
